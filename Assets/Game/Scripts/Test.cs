84ee794451472a15d20011289c4d06e2caad327d
Refaldy Destian Nur Pertala <refaldydestian1412@gmail.com>
2026-06-03 21:06:31 +0700

    feat: add Helllo World

--- Assets/Game/Scripts/Test.cs
+++ Assets/Game/Scripts/Test.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    Console.WriteLine("Hello World!");
+
     void Start()
     {
         
